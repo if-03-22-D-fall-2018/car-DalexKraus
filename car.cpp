eae66693192d2da44d3013176f3e12a19c75c3b2
DalexKraus <dalex.kraus@gmail.com>
2018-12-17 16:42:39 +0100

    Finished Program.

---
+++ car.cpp
@@ -0,0 +1,127 @@
+/*----------------------------------------------------------
+*				HTBLA-Leonding / Klasse: 2DHIF
+* ---------------------------------------------------------
+* Exercise Number:  07
+* Title:			car.h
+* Author:			David Kraus
+* Due Date:		    December 17, 2018
+* ----------------------------------------------------------
+* Description:
+* Car abstract data type demo.
+* ----------------------------------------------------------
+*/
+#include "car.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUMBER_OF_CARS 6
+
+struct CarData
+{
+    enum CarType    type;
+    int             max_speed;
+    double          fill_level;
+    double          acceleration_rate;
+    double          speed;
+    double          lowest_acceleration_rate;
+    double          highest_acceleration_rate;
+};
+
+struct CarImplementation
+{
+    enum    Color      color;
+    struct  CarData    car_data;
+    bool               is_rented;
+};
+
+struct CarData aixam            = { AIXAM,          45,     16.0, 0.0, 0, -8.0, 1.0  };
+struct CarData fiat_multipla    = { FIAT_MULTIPLA,  170,    65.0, 0.0, 0, -8.0, 2.26 };
+struct CarData jeep             = { JEEP,           196,    80.0, 0.0, 0, -8.0, 3.14 };
+
+struct CarImplementation aixam1         = { RED,    aixam,          false };
+struct CarImplementation fiat_multipla1 = { GREEN,  fiat_multipla,  false };
+struct CarImplementation fiat_multipla2 = { BLUE,   fiat_multipla,  false };
+struct CarImplementation fiat_multipla3 = { ORANGE, fiat_multipla,  false };
+struct CarImplementation jeep1          = { SILVER, jeep,           false };
+struct CarImplementation jeep2          = { BLACK,  jeep,           false };
+
+static Car car_park[] = {
+    &aixam1,
+    &fiat_multipla1,
+    &fiat_multipla2,
+    &fiat_multipla3,
+    &jeep1, &jeep2
+};
+
+void init()
+{
+    for (int i = 0; i < NUMBER_OF_CARS; i++)
+    {
+        car_park[i]->is_rented                  = false;
+        car_park[i]->car_data.speed             = 0;
+        car_park[i]->car_data.acceleration_rate = 0;
+    }
+}
+
+Car get_car(enum CarType type)
+{
+    for (int i = 0; i < NUMBER_OF_CARS; i++)
+    {
+        if (car_park[i]->car_data.type == type && !car_park[i]->is_rented)
+        {
+            car_park[i]->is_rented = true;
+            return car_park[i];
+        }
+    }
+    return 0;
+}
+
+double get_fill_level(Car car)
+{
+    return car->car_data.fill_level;
+}
+
+double get_acceleration_rate(Car car)
+{
+    return car->car_data.acceleration_rate;
+}
+
+void set_acceleration_rate(Car car, double acceleration_rate)
+{
+    if (acceleration_rate >= car->car_data.lowest_acceleration_rate && acceleration_rate <= car->car_data.highest_acceleration_rate)
+        car->car_data.acceleration_rate = acceleration_rate;
+    else if (acceleration_rate < car->car_data.lowest_acceleration_rate)
+        car->car_data.acceleration_rate = car->car_data.lowest_acceleration_rate;
+    else car->car_data.acceleration_rate = car->car_data.highest_acceleration_rate;
+}
+
+void accelerate(Car car)
+{
+    double temp = car->car_data.acceleration_rate * 3.6;
+
+    if (temp + car->car_data.speed < car->car_data.max_speed)
+        car->car_data.speed+=temp;
+    else if (car->car_data.speed < car->car_data.max_speed)
+    {
+        double delta = car->car_data.max_speed - car->car_data.speed;
+        car->car_data.speed += delta;
+    }
+}
+
+int get_speed(Car car)
+{
+    if (car->car_data.speed - (int) car->car_data.speed >= 0.5) {
+        return (int) car->car_data.speed + 1;
+    }
+    return (int) car->car_data.speed;
+}
+
+enum CarType get_type(Car car)
+{
+    return car->car_data.type;
+}
+
+enum Color get_color(Car car)
+{
+    return car->color;
+}
--- car.h
+++ car.h
@@ -1,10 +1,10 @@
 /*----------------------------------------------------------
-*				HTBLA-Leonding / Klasse: <your class>
+*				HTBLA-Leonding / Klasse: 2DHIF
 * ---------------------------------------------------------
-* Exercise Number: 0
+* Exercise Number:  07
 * Title:			car.h
-* Author:			P. Bauer
-* Due Date:		January 9, 2015
+* Author:			David Kraus
+* Due Date:		    December 17, 2018
 * ----------------------------------------------------------
 * Description:
 * Car abstract data type demo.
@@ -12,5 +12,30 @@
 */
 #ifndef ___CAR_H
 #define ___CAR_H
+
+typedef struct CarImplementation* Car;
+
+enum CarType
+{
+    AIXAM, FIAT_MULTIPLA, JEEP
+};
+
+enum Color
+{
+    RED, GREEN, BLUE, ORANGE, SILVER, BLACK
+};
+
+//Public functions
+//
+void init();
+Car get_car(enum CarType type);
+double get_fill_level(Car car);
+double get_acceleration_rate(Car car);
+void set_acceleration_rate(Car car, double acceleration_rate);
+void accelerate(Car car);
+int get_speed(Car car);
+
+enum CarType get_type(Car car);
+enum Color get_color(Car car);
 
 #endif
